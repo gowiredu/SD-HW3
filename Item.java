b00083181f6690f62b82c66de71daa64f4508070
gowiredu <gordon.kwame@gmail.com>
2015-02-26 18:29:44 -0500

    new
    new

---
+++ Item.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+ 
+/*
+* This class is modeling a basic 
+* adventure game.  
+*
+* @author: Justine Clarke & Gordon Owiredu
+*/
+
+public class Item {
+
+private boolean consumable; //Is this Item consumable (T) or not (F)
+private string shortName; 
+private string description ?;
+
+//CONSTRUCTROR
+public Item( String ? ){
+? = the?; 
+}
+
+// The ToString method which returns the output of the name, type and description of the Item
+public String toString() {
+String output = new String();
+
+output =  
+return output; 
+}
+
+public static void main (Strings [] args) {
+
+}
+
+
+
+
+
+
+}
